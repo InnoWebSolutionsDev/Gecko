7b6701ad8798b1bde1931b06e6624dcb4e9cd592
mlobeto <mercedeslobeto@gmail.com>
2025-05-03 11:06:04 -0300

    uno

---
+++ LandingGecko/src/Components/Landing.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Login from './Auth/Login'
+
+
+
+
+const Landing = () => {
+  return (
+    <div>
+     <Login />
+     
+      </div>
+  )
+}
+
+export default Landing
