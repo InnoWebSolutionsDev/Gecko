6bedd12907c0e6105792f0a8c242a46cd3518170
mlobeto <mercedeslobeto@gmail.com>
2025-05-05 10:33:57 -0300

    tc

--- LandingGecko/tailwind.config.js
+++ LandingGecko/tailwind.config.js
@@ -1,11 +1,17 @@
-export default {
-    theme: {
-      extend: {
-        fontFamily: {
-          bebas: ['"Bebas Neue"', 'cursive'],
-        },
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        bebas: ['"Bebas Neue"', 'cursive'],
       },
     },
-    content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
-    plugins: [],
-  }
+  },
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  plugins: [],
+  safelist: [
+    'w-20',
+    'w-64',
+    'border-green-500',
+    'border-transparent',
+  ],
+};
